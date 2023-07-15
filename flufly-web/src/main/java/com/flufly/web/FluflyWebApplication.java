9f5b3c93cd9334758b554e19f79ee929888aa8e7
Rafael Solano <rafaelsolanol007@gmail.com>
2023-07-15 14:29:18 -0500

    Primer Commit

---
+++ flufly-web/src/main/java/com/flufly/web/FluflyWebApplication.java
@@ -0,0 +1,13 @@
+package com.flufly.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluflyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluflyWebApplication.class, args);
+	}
+
+}
---
+++ flufly-web/src/test/java/com/flufly/web/FluflyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flufly.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluflyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
